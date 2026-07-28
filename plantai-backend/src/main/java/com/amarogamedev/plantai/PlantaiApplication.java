17c0b6304a2a4f5b602871cd76cca65394b7709d
amaro <luisfellipeamaro@gmail.com>
2026-07-28 19:05:46 -0300

    initial commit

---
+++ plantai-backend/src/main/java/com/amarogamedev/plantai/PlantaiApplication.java
@@ -0,0 +1,13 @@
+package com.amarogamedev.plantai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantaiApplication.class, args);
+	}
+
+}
---
+++ plantai-backend/src/test/java/com/amarogamedev/plantai/PlantaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amarogamedev.plantai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
